07e2508d443f681248aed9caff2173c613194367
diegotf30 <diegotf30@gmail.com>
2021-01-17 18:37:38 -0600

    fix syntax error

--- a1/backend/app/controllers/auth/registrations_controller.rb
+++ a1/backend/app/controllers/auth/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Auth::RegistrationsController < Devise::RegistrationsController
   protect_from_forgery with: :null_session
-  skip_before_action :verstify_authenticity_token
+  skip_before_action :verify_authenticity_token
   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
